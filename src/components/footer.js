2d53bfd86099e5fdebc309cd986fa778aa6771bd
Mfundo <mfundoknox@gmail.com>
2025-07-14 12:36:07 +0200

    links

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
   const handleSocialClick = (platform) => {
     const urls = {
       facebook: 'https://www.facebook.com/BothakaBjaMahlako',
-      whatsapp: 'https://wa.me/27659328906',
-      location: "https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d12819.912951207127!2d29.489362794426704!3d-24.31402321246719!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sza!4v1752253201142!5m2!1sen!2sza"
+      whatsapp: 'https://wa.me/277659328906',
+      location: "https://maps.app.goo.gl/goybeqWZtPNdCZqJA"
     };
     window.open(urls[platform], '_blank');
   };
@@ -41,7 +41,7 @@ const Footer = () => {
     rel="noopener noreferrer"
     style={{ color: '#25D366', textDecoration: 'none' }}
   >
-    Mfundo
+    Mfundodev.com
   </a>.
 </p>
     </footer>
